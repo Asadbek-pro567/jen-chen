6cc85be9e816a5d1582da0fccf5d0f59b0225f15
KomilovIslombek <islambek445@gmail.com>
2023-02-01 08:30:40 +0500

    work

--- src/components/Facts/Fact.jsx
+++ src/components/Facts/Fact.jsx
@@ -30,7 +30,7 @@ const Fact = () => {
                             </div>
                             <div className="fact__content">
                                 <div className="content__price">
-                                    1.3 <span id="price_desc">mlrd</span>
+                                    1.3 <p id="price_desc">mlrd</p>
                                 </div>
                                 <div className="content__info">
                                     1,3 mlrd xitoyliklar 
