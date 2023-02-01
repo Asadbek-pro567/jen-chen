2bfa901bed6add357b057e1521570b00106cf1b8
Asadbek-pro567 <masadbek2006@gmail.com>
2023-02-01 17:08:57 +0500

    zb

--- src/components/Turlari/Turlari.jsx
+++ src/components/Turlari/Turlari.jsx
@@ -2,14 +2,16 @@ import React from 'react'
 import './Turlari.css'
 import turlari from '../../assets/img/uy.png'
 import jenshen from '../../assets/img/jenshen.png'
+import Uy from '../../assets/img/uy.png'
+
 function Turlari() {
     return (
         <div className='turlari' id='mahsulot'>
-            <div className="mycontainer">
+            <div className="myycontainer">
                 <div className="js">
-                    <ul className="uy">
+                    <ul className="uyyyyy">
                         <li>
-                            <img src={jenshen} alt="" />
+                            <img src={jenshen} alt="" className='uy2'/>
                             <h2>Qizil jen shen 1 199 000</h2>
                         </li>
                         <li>
