1b5a96a34d3f6e86983d88c6317ba8ab0456b35f
KomilovIslombek <islambek445@gmail.com>
2023-02-03 15:42:16 +0500

    12

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,8 +5,30 @@ import footerImage from '../../assets/img/footerImage.png';
 import logo from '../../assets/img/logo.png';
 
 const Footer = () => {
+
+    const formReg = (e) => {
+        e.preventDefault()
+        const el = e.target.elements
+        fetch('https://638208329842ca8d3c9f7558.mockapi.io/get-zapros', {
+          method: 'POST',
+          headers: {
+            'Content-type': 'application/json', // qysi formatta yuborish
+            'Accept': 'application/json', // qysi formatta uni qabul qilib olishi
+            'Access-Control-Allow-Origin': '*' // ruxsat berish hammaga
+          },
+          body: JSON.stringify({
+            name: el.name.value,
+            tel: parseInt(el.tel.value)
+          })
+        })
+          .then((res) => res.json())
+          .then((data) => console.log(data))
+        e.target.elements.name.value = ''
+        e.target.elements.tel.value = '998'
+      }
+
     return (
-        <div  className='Footer'>
+        <div  className='footer'>
             <div className="mycontainerrr">
                 <div className="footer__row">
                     <div className="footer__contact">
@@ -38,10 +60,10 @@ const Footer = () => {
                         <div className="component"><a href="#mahsulot">MAHSULOT</a></div>
                     </div>
                     <div className="footer__form">
-                        <form action="#" method='post' className="form">
+                        <form onSubmit={formReg} action="#" method='post' className="form">
                             <div className="form__title">Formani toʻldiring, mutaxascislardan bepul konsultatsiya oling va chegirmaga ega boling</div>
-                            <input type="text" placeholder='Ismingiz' />
-                            <input type="text"  placeholder='raqam' />
+                            <input type="text" name='name' placeholder='Ismingiz' />
+                            <input type="text" name='tel'  placeholder='raqam' />
                             <button type='submit'>Buyurtma berish</button>
                         </form>
                     </div>
